6d0c3f73da97a81348147cc999d4523b62be7e12
Zamiul-rashid <134862075+Zamiul-rashid@users.noreply.github.com>
2023-11-10 16:22:37 +0600

    Removed useless files

--- Password generator/exe file fist try/setup.py
+++
@@ -1,9 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Pass_Gen",
-    version = "1.0",
-    description = "Password gen",
-    executables = [Executable("password_generator.py")]
-)
